97ef73bbbdfed2e0f1a30b76b69411fe1f75e8eb
brunxu <86543727+brunxu@users.noreply.github.com>
2021-06-26 22:23:23 -0600

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -203,7 +203,7 @@ onMount( async () => {
 	</table>
 	{/if}
 	<blockquote>
-		Trades profit are calculate on a base budget of 100USD and a 0.0075% fee
+		Trades profit are calculate on a base budget of 100USD and a 0.075% fee
 	</blockquote>
 
 	{#if oldWinners && oldWinners.length}
